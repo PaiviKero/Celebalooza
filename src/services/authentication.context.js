2c0adeb2d74dfbfee5e77a508710808e590a175e
Päivi Kero <paivi.kero@gmail.com>
2023-01-16 14:11:47 +0100

    initiliazeLanguage

--- src/services/authentication.context.js
+++ src/services/authentication.context.js
@@ -1,12 +1,16 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
+
+import { LocalizationContext } from "../services/localization.context";
 
 export const AuthenticationContext = createContext();
 
 export const AuthenticationContextProvider = ({ children }) => {
-  const [user, setUser] = useState("defUser");
+  const { initializeAppLanguage } = useContext(LocalizationContext);
+  const [user, setUser] = useState(null);
 
   const onLogin = () => {
     setUser("defUser");
+    initializeAppLanguage();
   };
 
   return (
