ad96e659f361535e7e4e2195bf808161b13fa869
Päivi Kero <paivi.kero@gmail.com>
2023-01-19 12:13:05 +0100

    autologin

--- src/services/authentication.context.js
+++ src/services/authentication.context.js
@@ -1,15 +1,31 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { LocalizationContext } from "../services/localization.context";
 
 export const AuthenticationContext = createContext();
 
+const LOGIN_KEY = "login_id";
+
 export const AuthenticationContextProvider = ({ children }) => {
   const { initializeAppLanguage } = useContext(LocalizationContext);
   const [user, setUser] = useState(null);
 
-  const onLogin = () => {
-    setUser("defUser");
+  useEffect(() => {
+    const loadUser = async () => {
+      let storedUser = await AsyncStorage.getItem(LOGIN_KEY);
+      setUser(storedUser);
+    };
+    loadUser();
+  });
+
+  const onLogin = async () => {
+    if (!user) {
+      let loginAs = "defUser";
+      setUser(loginAs);
+      AsyncStorage.setItem(LOGIN_KEY, loginAs);
+    }
+
     initializeAppLanguage();
   };
 
